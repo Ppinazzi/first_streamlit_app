fb947c9cc85213b734436fa49e82e49f26e47595
Pietro Pinazzi <63447659+Ppinazzi@users.noreply.github.com>
2023-09-23 18:43:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,3 +17,5 @@ df_fruit_list = pd.read_csv(filepath)
 streamlit.multiselect("Pick your fruits:", list(df_fruit_list.index))
 # Show the fruit list below the pick up list:
 streamlit.dataframe(df_fruit_list)
+
+streamlit.dataframe(df_fruit_list.iloc[:, "Fruit"])
