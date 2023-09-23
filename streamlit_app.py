4cc1e49aff73edc5606294b943ed27911e7c719f
Pietro Pinazzi <63447659+Ppinazzi@users.noreply.github.com>
2023-09-23 18:22:55 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,3 @@
 import streamlit
 
-streamlit.title("Hello World!")
-streamlit.header("Prova gli header")
-streamlit.text("Prova text")
-streamlit.title("Hello World!")
+streamlit.header("'🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
