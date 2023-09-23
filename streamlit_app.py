f2ec1b04ac0cc8f22e8d50d2e354c7f8f40a638f
Pietro Pinazzi <63447659+Ppinazzi@users.noreply.github.com>
2023-09-23 18:26:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,10 @@
 import streamlit
 
+streamlit.header('Breakfast Favorites')
+
+streamlit.text("🥣 Omega 3 & Blueberry Oatmel")
+streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
+streamlit.text("🐔 Chicken !!")
+streamlit.text("🥑🍞 Avodacdo Toast")
+
 streamlit.header("'🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
