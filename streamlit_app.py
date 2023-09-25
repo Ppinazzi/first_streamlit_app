35db8ca82660bfd5a856786a7121bb41ff8b9c6c
Pietro Pinazzi <63447659+Ppinazzi@users.noreply.github.com>
2023-09-25 15:27:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,4 @@ streamlit.header("Suggestion of the week:")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response.json())
 streamlit.dataframe(pd.json_normalize(fruityvice_response.json()))
+#
